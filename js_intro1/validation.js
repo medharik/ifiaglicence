1f00146faed8d69f2024c7846782280d90c57017
unknown <med.harik@gmail.com>
2019-01-04 21:05:55 +0000

    cours js

---
+++ js_intro1/validation.js
@@ -0,0 +1,23 @@
+// function valider(){
+// var cin=document.getElementById('cin');
+// var tel=document.querySelector('#tel');
+// var erreur=document.querySelector('#erreur');
+
+//  if(cin.value.trim()==""){
+// //alert(cin.name+' est obligatoire');
+// erreur.innerHTML=cin.name+' est obligatoire';
+// erreur.style.display='block'; 
+// }else
+//  if(tel.value.trim()==""){
+//     erreur.innerHTML="tel est obligatoire";
+//     erreur.style.display='block'; 
+//  }else{
+//     erreur.style.display='none'; 
+
+//  }
+
+
+
+// }
+
+
